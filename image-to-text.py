8fb549c87ff94ed6949c524fb6a8d29b104f6cc4
Vu Doan <victordoan1708@gmail.com>
2020-05-05 21:39:57 +0100

    SUCCESS IN LOADING MULTIPLE LANGUAGES. GRAYED OUT THE SUBJECT FOR BETTER OCR

--- image-to-text.py
+++ image-to-text.py
@@ -5,28 +5,36 @@ import pytesseract as pt
 # https://github.com/UB-Mannheim/tesseract/wiki?fbclid=IwAR2aj_N_2qrLLyNFRYwULr_1NDaB20TPQa93h-beGDvIQv1akGsByEXYCOQ
 
 # then add tesseract to the path
-pt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# pt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # import python imaging library
 from PIL import Image
 
-# import opencv
 import cv2 as cv
 
 # import numpy as np
-# print('Package Loaded')
+
+langs = 'vie+eng+kor+chi_sim+fra+jpn'
 # img = Image.open('media/img/3.jpg')
 cap = cv.VideoCapture(0)
 
 while True:
     success, img = cap.read()
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     cv.imshow('Live Webcam', img)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
-    toText = pt.image_to_string(img)
+    toText = pt.image_to_string(gray, lang=langs)
     if toText == '' or toText == None:
         pass
     else:
         print(toText)
 
+# able to run and detect SOME text, but because of really high latency (lagging), it's hard for OCR to recognise live webcam
+
+# testing for other languages with still images (SUCCEEDED)
+# langs = 'vie+eng+kor+chi_sim+fra'
+# img = Image.open('media/img/fra.png')
+# toText = pt.image_to_string(img, lang=langs)
+# print(toText)
